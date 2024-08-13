9e15aa358dc6823484176f2993ff1fd06a72ad74
Abdurashid <abdulllayevabdurashid@gmail.com>
2024-08-13 09:44:59 +0500

    added language changer

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer');
-const TelegramBot = require('node-telegram-bot-api'); // Assuming you have TelegramBot installed
+const TelegramBot = require('node-telegram-bot-api');
 
 // Replace with your Telegram bot token
 const telegramToken = '6051605197:AAGsf2rHujh_8W8GAEayNSRnyliGnNFH18A';
 const bot = new TelegramBot(telegramToken);
 const userIds = ["1215214465"];
+
 function sendMessageToAllUsers(message) {
   userIds.forEach(async (userId) => {
     try {
@@ -22,11 +23,18 @@ async function delay(ms) {
 
 async function checkAppointmentAvailability() {
   try {
-    const browser = await puppeteer.launch({ headless: true }); // Set headless to false for debugging
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('https://appointment.bmeia.gv.at/');
 
-    // Wait for the page to load and elements to become available
+    // Set language to Russian
+    await page.waitForSelector('#Language');
+    await page.select('#Language', 'ru');
+    await page.click('input[name="Command"]');
+    await delay(1000);
+
+    // Proceed with the rest of the form interaction
+
     await page.waitForSelector('#Office');
     
     // Select "Maskau" from the dropdown
@@ -45,15 +53,13 @@ async function checkAppointmentAvailability() {
 
     await page.waitForSelector('input[name="Command"]');
     await page.click('input[value="дальше"]');
-    await delay(2000); // Wait for 2 seconds
+    await delay(500); // Wait for 2 seconds
 
     await page.waitForSelector('input[name="Command"]');
     await page.click('input[value="дальше"]');
-    await delay(2000); // Wait for 2 seconds
+    await delay(1000); // 
 
-
-    // Wait for the response to load
-
+    // Check if the table with class 'no-border' exists
     const errorMessage = await page.evaluate(() => {
       const errorElement = document.querySelector('.message-error');
       return errorElement ? errorElement.textContent.trim() : '';
@@ -64,6 +70,7 @@ async function checkAppointmentAvailability() {
       sendMessageToAllUsers('Appointments are available! Finally, check the website for details.');
     }
 
+    await page.screenshot({ path: 'debug_screenshot.png' });
     await browser.close();
   } catch (error) {
     console.error('Error:', error);
